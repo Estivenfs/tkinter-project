11001a811e3d1994f24d4afe86a4f1c59bbc235c
estiven alexander fernandez soto <estivenfs@gmail.com>
2023-08-15 20:40:54 -0300

    buton and frame added

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def home():
     )
     home_label.grid(row=0, column=0, columnspan=2)
     add_label.grid_remove()
+    add_frame.grid_remove()
    
 
 def add():
@@ -33,6 +34,7 @@ def add():
     add_label.grid(row=0, column=0, columnspan=3, sticky=W)
 
     #Campos del formulario
+    add_frame.grid(row=1)
     add_name_label.grid(row=1, column=0, padx=5, pady=5, sticky=W)
     add_name_entry.grid(row=1, column=1, padx=5, pady=5, sticky=W)
 
@@ -41,6 +43,16 @@ def add():
 
     add_description_label.grid(row=3, column=0, padx=5, pady=5, sticky=W)
     add_description_entry.grid(row=3, column=1, padx=5, pady=5, sticky=W)
+
+    add_separator.grid(row=4, column=1)
+
+    button.grid(row=5, column=1, sticky=NW)
+    button.config(
+        padx=15,
+        pady=5,
+        bg="black",
+        fg="white"
+    )
     home_label.grid_remove()
 
 #variables importantes
@@ -56,16 +68,18 @@ home_label = Label(win, text="Home")
 add_label = Label(win, text="Añadir producto")
 
 #Campos de formulario
-add_name_label = Label(win, text="Nombre del producto:")
-add_name_entry = Entry(win, textvariable="name_data")
+add_frame = Frame(win)
+
+add_name_label = Label(add_frame, text="Nombre del producto:")
+add_name_entry = Entry(add_frame, textvariable="name_data")
 add_name_entry.focus()
 add_name_entry.config(justify="left", state="normal")
 
-add_price_label = Label(win, text="Precio del producto:")
-add_price_entry = Entry(win, textvariable="price_data")
+add_price_label = Label(add_frame, text="Precio del producto:")
+add_price_entry = Entry(add_frame, textvariable="price_data")
 
-add_description_label = Label(win, text="Descripción del producto:")
-add_description_entry = Text(win)
+add_description_label = Label(add_frame, text="Descripción del producto:")
+add_description_entry = Text(add_frame)
 add_description_entry.config(
     width=30,
     height=5,
@@ -73,6 +87,10 @@ add_description_entry.config(
     padx=5,
     pady=5
 )
+
+add_separator = Label(add_frame)
+
+button = Button(add_frame, text="Guardar")
 
 #Cargar la pantalla de inicio
 home()
